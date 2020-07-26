af945a814b1e9fa2cf0e17054110da0df3079e6b
RevanthR <revanth.rallabandi@gmail.com>
2020-07-27 00:19:15 +0530

    HomePage Completed

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Book, Author, BookInstance, Genre
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello welcome to your first view')
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_books_filter = Book.objects.filter(title__icontains='Half').count()
+    context = {
+        'num_books':num_books,
+        'num_instances': num_instances,
+        'num_instances_available':num_instances_available,
+        'num_authors': num_authors,
+        'num_genres' : num_genres,
+        'num_books_filtered': num_books_filter,
+    }
+
+    return render(request,'index.html',context=context)
